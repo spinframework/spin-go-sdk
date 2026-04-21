22771cfc5be54c89fd4c18d6261ebde97d0d91f5
Adam Reese <adam@reese.io>
2026-04-21 11:37:22 -0700

    Add a temp function for creating the database in the example
    Signed-off-by: Adam Reese <adam@reese.io>

--- examples/pg-outbound/main.go
+++ examples/pg-outbound/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"slices"
 	"time"
 
 	spinhttp "github.com/spinframework/spin-go-sdk/v3/http"
@@ -29,6 +31,21 @@ func init() {
 		db := pg.Open(addr)
 		defer db.Close()
 
+		if err := setupDB(db); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var x []int32
+		if err := db.QueryRow(`SELECT ARRAY[200, 404]`).Scan(&x); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if !slices.Equal(x, []int32{200, 404}) {
+			http.Error(w, fmt.Sprintf("Slices aren't equal, got: %v", x), http.StatusInternalServerError)
+			return
+		}
+
 		_, err := db.Exec("INSERT INTO pets (id, name, prey, is_finicky, timestamp) VALUES ($1, 'Maya', $2, $3, $4);", int32(4), "bananas", true, time.Now())
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -51,6 +68,33 @@ func init() {
 		}
 		json.NewEncoder(w).Encode(pets)
 	})
+}
+
+// temp function to make developing types easier.
+func setupDB(db *sql.DB) error {
+	if _, err := db.Exec(`DROP TABLE pets`); err != nil {
+		return err
+	}
+	if _, err := db.Exec(`CREATE TABLE pets (
+	  id INT PRIMARY KEY,
+	  name VARCHAR(100) NOT NULL,
+	  prey VARCHAR(100),
+	  is_finicky BOOL NOT NULL,
+	  timestamp TIMESTAMP
+	)`); err != nil {
+		return err
+	}
+	if _, err := db.Exec(`INSERT INTO pets VALUES (1, 'Splodge', NULL, false, '2026-04-20 12:30:00')`); err != nil {
+		return err
+	}
+	if _, err := db.Exec(`INSERT INTO pets VALUES (2, 'Kiki', 'Cicadas', false, '2026-04-20 12:30:00')`); err != nil {
+		return err
+	}
+	if _, err := db.Exec(`INSERT INTO pets VALUES (3, 'Slats', 'Temptations', true, '2026-04-20 12:30:00')`); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {}
